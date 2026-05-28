a6b86f9c6b5e5d00680f6003a2b6aceb9d90ea2c
Samarth-Agarwal24027 <24027@burside.school.nz>
2026-05-28 12:36:17 +1200

    Small change

--- Python/dungeon-practice.py
+++ Python/dungeon-practice.py
@@ -1,21 +1,24 @@
 party_power = 12
-dungeon = []
+dungeons = []
+
 while True:
-    name = input("Enter the Dungeon Name: ")
+    name = input("Enter Dungeon Name: ")
     while True:
+        level_input = input("Enter the Dungeon level: ")
         try:
-            level = int(input("Enter the Dungeon Level: "))
+            level = int(level_input)
             if level > 0:
                 break
             else:
                 print("Enter a positive number. Try again.")
         except:
-            print("Invalid input. Try again.")
-    dungeon.append((name, level))
+            print("Invalid Dungeon level. Try again: ")
+    dungeons.append((name, level))
     if name == "Polaris":
         break
-    for name, level in dungeon:
-        if level <= party_power:
-            print("We can confidently explore " + name + ".")
-        else:
-            print("We cannot confidently explore " + ".")
+
+for name, level in dungeons:
+    if level <= party_power:
+        print("We can confidently explore " + name + "." )
+    else:
+        print("We can confidently explore " + name + ".")
